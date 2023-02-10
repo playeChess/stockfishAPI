8dcac9c767527c5df7429363faa5a43ad7e851f9
Jasiukiewicz Tymon <JasiukiewiczTymon@outlook.fr>
2023-02-10 11:17:45 +0100

    🐟 Stockfish installer

---
+++ src/stockfish-install.sh
@@ -0,0 +1,8 @@
+# Stockfish installation (to execute with sudo)
+# Create for docker
+# playechess.com
+
+apt-get update
+apt-get install -y git make g++ wget
+git clone https://github.com/official-stockfish/Stockfish
+cd Stockfish/src && CXXFLAGS='-march=native' make -j2 profile-build ARCH=x86-64-bmi2 && cp ./stockfish /bin
