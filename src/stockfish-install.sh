a92b90f733b5d3908e93b4d1a2be8a84033f7bce
Jasiukiewicz Tymon <JasiukiewiczTymon@outlook.fr>
2023-02-28 13:06:40 +0100

    Delete stockfish-install.sh

--- src/stockfish-install.sh
+++
@@ -1,9 +0,0 @@
-# Stockfish installation (to execute with sudo)
-# Create for docker
-# playechess.com
-# Any ARM support
-
-apt-get update
-apt-get install -y git make g++ wget
-git clone https://github.com/official-stockfish/Stockfish
-cd Stockfish/src && CXXFLAGS='-march=native' make -j2 profile-build ARCH=x86-64-bmi2 && cp ./stockfish /bin
